6675b222fcc624c399d488a558724612a7ab382f
nindanaoto <dirty.knife603@gmail.com>
2022-09-10 20:21:37 +0900

    Added wrapper4utest01

---
+++ test/wrapper4testu01.cpp
@@ -0,0 +1,29 @@
+// Adapted from TestU01 manual, Figure 2.2, Figure 2.4
+#include <random>
+#include <string>
+#include <cstdio>
+#include <cstdint>
+#include <cstddef>
+
+extern "C" {
+    #include "TestU01.h"
+}
+
+#include <xoodoo-prng.hpp>
+
+XoodooPRNG::XoodooPRNG<uint32_t> rng(42);
+uint32_t Rand32(){return rng();}
+
+int main()
+{
+    // Create TestU01 PRNG object for our generator
+    unif01_Gen* gen = unif01_CreateExternGenBits("Xoodoo-PRNG", Rand32);
+
+    // Run the tests.
+    bbattery_SmallCrush(gen);
+
+    // Clean up.
+    unif01_DeleteExternGenBits(gen);
+
+    return 0;
+}
